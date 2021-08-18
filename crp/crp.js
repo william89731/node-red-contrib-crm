9df671c0f53e125d1d4152f9f43d64fa6817f91f
william89731 <68069659+william89731@users.noreply.github.com>
2021-08-18 05:05:29 +0200

    Update crp.js

--- crp/crp.js
+++ crp/crp.js
@@ -12,10 +12,28 @@ module.exports = function(RED) {
 			var autoreset = msg.hasOwnProperty("autoreset")? msg.autoreset : config.autoreset;
             var nodeContext = this.context();
             var count=nodeContext.get('count') || 0;	
-			
+			var timeout = msg.hasOwnProperty("timeout")? msg.timeout : config.timeout;
+			var max_timeout = msg.hasOwnProperty("max_timeout")? msg.max_timeout : config.max_timeout;
 				
-				
-				if (msg.payload === "STOP" || msg.payload === "stop") 
+
+			if (timeout===true)
+			{
+			setTimeout(() => {
+					nodeContext.set('count',0);
+				},max_timeout);
+		    }		
+			else if (timeout===false)
+			{
+					clearTimeout(() => {
+						    nodeContext.set('count',count);
+					    },max_timeout);
+
+			}
+
+
+
+
+			    if (msg.payload === "STOP" || msg.payload === "stop") 
 				{                    
                   
                     node.status({fill:"red",shape:"dot",text:count});
@@ -29,7 +47,13 @@ module.exports = function(RED) {
                     msg = null;
                     node.status({fill:"yellow",shape:"dot",text:count});
                     this.send([ null , null ]);
-                } 
+                }
+				else if (timeout=== 0 )
+                {
+                    
+                    
+					clearTimeout;   
+                }    
 				else 
 				{
                     count = count+1;
@@ -66,4 +90,3 @@ module.exports = function(RED) {
 		console.error("STI GRAN CAXXI");
 	}
 }
-
